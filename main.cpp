6cb268df2db21ed7a6054a8dc45c811cb0dd428e
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-02 12:39:31 -0600

    Add file to project and build it

--- main.cpp
+++ main.cpp
@@ -1,30 +1,10 @@
 #include <iostream>
 
+int add(int x, int y);
 
-int doubleNumber(int num);
-int tripleNumber(int num);
-
-
-// Lesson 2.4, Question 5: Write a complete program that reads an integer from the user,
-//   doubles it using the doubleNumber() function you wrote in the previous quiz
-//   question, and then prints the doubled value out to the console.
+// Lesson 2.9, Practice calling functions located in another file
 int main()
 {
-	std::cout << "Enter an integer: ";
-	int user_input{ };
-	std::cin >> user_input;
-
-	std::cout << "Double " << user_input << " is: " << doubleNumber(user_input) << '\n';
-	std::cout << "Triple " << user_input << " is: " << tripleNumber(user_input) << '\n';
-	return 0;
-}
-
-int doubleNumber(int num)
-{
-	return num * 2;
-}
-
-int tripleNumber(int num)
-{
-    return num *3;
+	std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
+    return 0;
 }
