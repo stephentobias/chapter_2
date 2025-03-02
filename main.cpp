a12799ffb857b243d6c627988cacf2d1c8fbf81b
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-02 12:45:47 -0600

    Answer quiz question in lesson 2.8
    Split give program into separate files and compile it successfully

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
-int add(int x, int y);
+// Lesson 2.8, quiz
+int getInteger();
 
-// Lesson 2.9, Practice calling functions located in another file
 int main()
 {
-	std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
-    return 0;
+	int x{ getInteger() };
+	int y{ getInteger() };
+
+	std::cout << x << " + " << y << " is " << x + y << '\n';
+	return 0;
 }
