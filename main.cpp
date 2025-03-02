b358ab4d2ddbf75e8a27d63b22617bcaea459b9e
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-01 23:18:37 -0600

    Write program that prints double an integer
    Program prompts the user for an integer then prints double the input.
    This is practice for lesson 2.4; it uses a function that takes the user input as an argument then returns double the number back to the caller which then takes that returned value as input to std::cout.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+
+int doubleNumber(int num);
+
+
+// Question 5: Write a complete program that reads an integer from the user, 
+//   doubles it using the doubleNumber() function you wrote in the previous quiz 
+//   question, and then prints the doubled value out to the console.
 int main()
 {
-	std::cout << "Hello, world!" << '\n';
+	std::cout << "Enter an integer: ";
+	int user_input{ };
+	std::cin >> user_input;
+
+	std::cout << "Double " << user_input << " is: " << doubleNumber(user_input) << '\n';
 	return 0;
+}
+
+int doubleNumber(int num)
+{
+	return num * 2;
 }
