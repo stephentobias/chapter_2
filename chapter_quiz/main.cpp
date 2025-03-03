0ddc1064269d521aa7574a0a2e6d0cc25e31cf7d
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-03 08:59:56 -0600

    Update comment

--- chapter_quiz/main.cpp
+++ chapter_quiz/main.cpp
@@ -1,7 +1,7 @@
 /* Chapter 2 quiz question 3
-    Modify the program you wrote in #2 so that it uses a header file (named io.h)
-        to access the functions instead of using forward declarations directly
-        in your code (.cpp) files. Make sure your header file uses header guards.
+ * Modify the program you wrote in #2 so that it uses a header file (named io.h)
+ *  to access the functions instead of using forward declarations directly
+ *  in your code (.cpp) files. Make sure your header file uses header guards.
  */
 
 #include "io.h"
